a03ab4573b3da351efb5c492082e071509219e4f
manpreet-codespace <manpreet.dev.85@gmil.com>
2026-01-23 11:38:24 +0530

    new: print numbers in range

---
+++ Loops/print_number_1_to_N.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+// -------------WHILE LOOP------------
+
+int main(){
+
+    int num = 1;
+    int range;
+
+    cin>>range;
+
+    while(num <= range){
+        cout<<num<<endl;
+        num++;
+
+    }
+
+// ---------FOR LOOP-----------
+
+    cin>> range;
+
+    for(int num=1;num<=range;num++){
+        cout<<num<<endl;
+    }
+
+    return 0;
+
+}
